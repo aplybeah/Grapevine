ec361c4911a3a8817cefb63efaa3086f9bbd085c
Alsia Plybeah <l.a.plybeah@gmail.com>
2019-02-26 15:22:10 -0500

    added method override; delete button attempt; more form functions

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const parser = require("body-parser");
+const methodOverride = require("method-override");
 
 app.set("view engine", "hbs");
 app.use(parser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 app.use(require("./routes/index"));
 
 app.listen(3000, function(req, res) {
