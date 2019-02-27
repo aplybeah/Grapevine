d435790947fadc2d9c7ddbbef097b71d92ec1fc1
Alsia Plybeah <l.a.plybeah@gmail.com>
2019-02-27 11:05:50 -0500

    built out a possible link for comments

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { Article } = require("../models/index");
+const { Comment } = require("../models/index");
 
 router.get("/", function(req, res) {
   Article.find({}).then(articles => {
