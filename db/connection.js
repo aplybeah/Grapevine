05edd4d3cd14e57675619cc4e199dd5e1de1501e
Alsia Plybeah <l.a.plybeah@gmail.com>
2019-03-05 14:13:41 -0500

    added to heroku

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/grapevine");
-mongoose.Promise = Promise;
 
 if (process.env.NODE_ENV == "production") {
   mongoose.connect(process.env.DB_URL);
 } else {
   mongoose.connect("mongodb://localhost/todo");
 }
+
+mongoose.Promise = Promise;
+
 module.exports = mongoose;
